a16bbd8e8908bfe46740501a41a21a0ac5832112
root <root@starter.kryukov.local>
2022-04-07 22:08:19 +0430

    update certs on ubuntu in bashscript

--- scripts/nexus-nginxproxy.sh
+++ scripts/nexus-nginxproxy.sh
@@ -21,7 +21,40 @@
 # ....................................................................... #
 
 # Create certs directory
-mkdir ../certs
+CERT_PATH=/opt/certs
+USR=david
+
+mkdir -p $CERT_PATH
+
+
+openssl req  -subj "/C=IR/ST=Tehran/L=Tehran/O=IT/OU=Devops/CN=private.repo.com" -newkey rsa:4096 -nodes -sha256 -keyout $CERT_PATH/registry-key.key -x509 -days 1825 -out $CERT_PATH/ca.crt  -extfile <(printf "subjectAltName = DNS:private.repo.com")
+
+cd ../nginx/
+echo $PWD
+
+cp $CERT_PATH/ca.crt            ca.crt
+cp $CERT_PATH/registry-key.key  registry-key.key
+
+scp $CERT_PATH/ca.crt $USR@m1:/tmp/ && ssh david@m1 sudo mv /tmp/ca.crt /usr/local/share/ca-certificates/ && ssh $USR@m1  sudo update-ca-certificates
+scp $CERT_PATH/ca.crt $USR@m2:/tmp/ && ssh david@m2 sudo mv /tmp/ca.crt /usr/local/share/ca-certificates/ && ssh $USR@m2  sudo update-ca-certificates
+scp $CERT_PATH/ca.crt $USR@w1:/tmp/ && ssh david@w1 sudo mv /tmp/ca.crt /usr/local/share/ca-certificates/ && ssh $USR@w1  sudo update-ca-certificates
+scp $CERT_PATH/ca.crt $USR@w2:/tmp/ && ssh david@w2 sudo mv /tmp/ca.crt /usr/local/share/ca-certificates/ && ssh $USR@w2  sudo update-ca-certificates
+scp $CERT_PATH/ca.crt $USR@w3:/tmp/ && ssh david@w3 sudo mv /tmp/ca.crt /usr/local/share/ca-certificates/ && ssh $USR@w3  sudo update-ca-certificates
+
+
+
+docker build --no-cache -t nginx-nexus-proxy .
+
+cd ../
+echo $PWD
+
+# Run nginx and nexus containers
+docker-compose up -d
+
+#        ------------- Create /etc/docker/certs.d/private.repo.com/ on cluster node if continerd is docker -------------------
+#  ssh david@m1 sudo mkdir -p /etc/docker/certs.d/private.repo.com/ && scp david@192.168.254.10:/opt/certs/ca.crt /etc/docker/certs.d/private.repo.com/
+
+
 
 
 #openssl req -x509 -out ../certs/cert.crt -keyout ../certs/cert.key -newkey rsa:2048 -nodes -sha256 -subj '/CN=private.repo.com' -extensions EXT -config <( \
@@ -103,21 +136,4 @@ EOF
 #fi
 #
 
-cd ../nginx/
-echo $PWD
-
-# Making Build Context for Dockerfile
-#cp ../certs/nexus3.cert    nexuscert.crt
-#cp ../certs/nexus3-key.pem nexuskey.pem
-
-cp /certificates/ca.crt            ca.crt
-cp /certificates/registry-key.key  registry-key.key
-
 # Docker build nginx image
-docker build --no-cache -t nginx-nexusproxy .
-
-cd ../
-echo $PWD
-
-# Run nginx and nexus containers
-docker-compose up -d
--- scripts/rest.sh
+++ scripts/rest.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 rm -fr ../certs/ && echo "remove ../certs/ Dir"
+rm -fr /opt/certs/ && echo "remove ../certs/ Dir"
 docker stop $(docker ps -aq) && docker container prune -f
